5b88473c1493655ba9b377cb30240e0d0c31733c
deepak76901 <deepakb76901@gmail.com>
2023-07-30 14:35:15 +0530

    new commit

---
+++ operator.js
@@ -0,0 +1,16 @@
+// Question no. 3 
+
+// let num = 44;
+// if(num%2 == 0 && num%3==0){
+//     console.log('Number is divisible by 2 and 3')
+// }
+// else{
+//     console.log('Number is not divisilbe by 2 and 3')
+// }
+
+
+
+// Q5
+
+let age = 15;
+(age>18) ? console.log('You can Drive') : console.log('You cann\'ot drive ')
